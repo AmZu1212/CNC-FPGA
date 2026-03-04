2d6d49024757fbdf9d00b174dfa13974b788aa8e
Amir Zuabi <zuabiamir123@gmail.com>
2026-03-04 16:04:42 +0200

    removed redundant comments

--- Zedboard-CNC/Zedboard-CNC.srcs/sources_1/new/GCODE_Parser.v
+++ Zedboard-CNC/Zedboard-CNC.srcs/sources_1/new/GCODE_Parser.v
@@ -1,31 +1,10 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: Amir Zuabi & Alex Vassiliev
-// 
-// Create Date: 06/23/2025 12:48:40 AM
-// Design Name: 
-// Module Name: GCODE_Parser
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module GCODE_Parser(
     //Inputs
     input clk,
     input rst,
     input start,
     input load_next_line,
-    
+
     //Outputs
     output reg [7:0] next_speed,
     output reg signed [31:0] next_pos_x,
@@ -33,37 +12,37 @@ module GCODE_Parser(
     output reg signed [31:0] next_pos_z,
     output reg enable
     );
-    
-    localparam max_line = 905;//912;
+
+    localparam max_line = 905;//912; // *** refactor
     reg [15:0] curr_line;
-    reg [15:0] speed [0:max_line-1]; //[0:911]
+    reg [15:0] speed [0:max_line-1]; //[0:911] // remove 2d array + add support for previous.
     reg signed [20:0] pos_x [0:max_line-1];
     reg signed [20:0] pos_y [0:max_line-1];
     reg signed [20:0] pos_z [0:max_line-1];
-    
-    
+
+
     reg [31:0] risingedge_buffer;
-    
+
     reg start_prev;
     wire start_risingedge;
     assign start_risingedge = start && !start_prev;
-    
+
     reg [2:0] state;
     localparam IDLE     = 0;
     localparam RUNNING  = 1;
     localparam RETURN   = 2;
-    
+
     reg [1:0] return_state;
     localparam XY_CHECK     = 0;
     localparam Z_RAISE      = 1;
     localparam XY_HOME      = 2;
     localparam Z_LOWER      = 3;
-    
+
     localparam Z_SAFE_HEIGHT    = 5000; //microns
     localparam RETURN_SPEED_Z   = 20;   //mm/s
     localparam RETURN_SPEED_XY  = 60;
-    
-    
+
+
     always @(posedge clk) begin
         if (rst) begin
             enable <= 0;
@@ -71,26 +50,26 @@ module GCODE_Parser(
             start_prev <= 0;
             state <= IDLE;
             return_state <= XY_CHECK;
-            
+
             next_speed <= speed[0];
             next_pos_x <= pos_x[0];
             next_pos_y <= pos_y[0];
             next_pos_z <= pos_z[0];
-            
+
             risingedge_buffer <= 100000000;
         end else begin
             start_prev <= start;
             enable <= 0;
-            
+
             if (start_risingedge) begin
                 risingedge_buffer <= 100000000;
             end else if (risingedge_buffer > 0) risingedge_buffer <= risingedge_buffer - 1;
-            
+
             case (state)
                 IDLE    : begin
                     if (start_risingedge && !risingedge_buffer) state <= RUNNING;
                 end
-                
+
                 RUNNING : begin
                     enable <= 1;
                     if (start_risingedge && !risingedge_buffer) begin
@@ -114,11 +93,11 @@ module GCODE_Parser(
                         end
                     end
                 end
-                
+
                 RETURN  : begin
                     enable <= 1;
                     case (return_state)
-                        
+
                         XY_CHECK : begin
                             if (pos_x[curr_line] || pos_y[curr_line]) begin
                                 if (pos_z[curr_line] < Z_SAFE_HEIGHT) begin
@@ -130,7 +109,7 @@ module GCODE_Parser(
                                 return_state <= Z_LOWER;
                             end
                         end
-                        
+
                         Z_RAISE : begin
                             //curr_line was decremented to match the currently running line
                             //so this keeps same X/Y but raises Z
@@ -140,14 +119,14 @@ module GCODE_Parser(
                             next_speed <= RETURN_SPEED_Z;
                             if (load_next_line) return_state <= XY_HOME;
                         end
-                        
+
                         XY_HOME : begin
                             next_pos_x <= 0;
                             next_pos_y <= 0;
                             next_speed <= RETURN_SPEED_XY;
                             if (load_next_line) return_state <= Z_LOWER;
                         end
-                        
+
                         Z_LOWER : begin
                             next_pos_z <= 0;
                             next_speed <= RETURN_SPEED_Z;
@@ -157,53 +136,19 @@ module GCODE_Parser(
                                 return_state <= XY_CHECK;
                             end
                         end
-                        
                     endcase
                 end
-                
+
             endcase
         end
     end
-    
+
     initial begin
     //X Y Z F
     //Note F speed is in mm/sec, max tested speed = 90
-    
     $readmemh("pos_x.hex", pos_x);
     $readmemh("pos_y.hex", pos_y);
     $readmemh("pos_z.hex", pos_z);
     $readmemh("speed.hex", speed);
-    
-//    pos_x[000] = 000000; pos_y[000] = 000000; pos_z[000] = 05000; speed[000] = 40;
-//    pos_x[001] = 050000; pos_y[001] = 000000; pos_z[001] = 05000; speed[001] = 40;
-//    pos_x[002] = 050000; pos_y[002] = 000000; pos_z[002] = 00000; speed[002] = 40;
-//    pos_x[003] = 062941; pos_y[003] = 001704; pos_z[003] = 00000; speed[003] = 90;
-//    pos_x[004] = 075000; pos_y[004] = 006699; pos_z[004] = 00000; speed[004] = 90;
-//    pos_x[005] = 085355; pos_y[005] = 014645; pos_z[005] = 00000; speed[005] = 90;
-//    pos_x[006] = 093301; pos_y[006] = 025000; pos_z[006] = 00000; speed[006] = 90;
-//    pos_x[007] = 098296; pos_y[007] = 037059; pos_z[007] = 00000; speed[007] = 90;
-//    pos_x[008] = 100000; pos_y[008] = 050000; pos_z[008] = 00000; speed[008] = 90;
-//    pos_x[009] = 098296; pos_y[009] = 062941; pos_z[009] = 00000; speed[009] = 90;
-//    pos_x[010] = 093301; pos_y[010] = 075000; pos_z[010] = 00000; speed[010] = 90;
-//    pos_x[011] = 085355; pos_y[011] = 085355; pos_z[011] = 00000; speed[011] = 90;
-//    pos_x[012] = 075000; pos_y[012] = 093301; pos_z[012] = 00000; speed[012] = 90;
-//    pos_x[013] = 062941; pos_y[013] = 098296; pos_z[013] = 00000; speed[013] = 90;
-//    pos_x[014] = 050000; pos_y[014] = 100000; pos_z[014] = 00000; speed[014] = 90;
-//    pos_x[015] = 037059; pos_y[015] = 098296; pos_z[015] = 00000; speed[015] = 90;
-//    pos_x[016] = 025000; pos_y[016] = 093301; pos_z[016] = 00000; speed[016] = 90;
-//    pos_x[017] = 014645; pos_y[017] = 085355; pos_z[017] = 00000; speed[017] = 90;
-//    pos_x[018] = 006699; pos_y[018] = 075000; pos_z[018] = 00000; speed[018] = 90;
-//    pos_x[019] = 001704; pos_y[019] = 062941; pos_z[019] = 00000; speed[019] = 90;
-//    pos_x[020] = 000000; pos_y[020] = 050000; pos_z[020] = 00000; speed[020] = 90;
-//    pos_x[021] = 001704; pos_y[021] = 037059; pos_z[021] = 00000; speed[021] = 90;
-//    pos_x[022] = 006699; pos_y[022] = 025000; pos_z[022] = 00000; speed[022] = 90;
-//    pos_x[023] = 014645; pos_y[023] = 014645; pos_z[023] = 00000; speed[023] = 90;
-//    pos_x[024] = 025000; pos_y[024] = 006699; pos_z[024] = 00000; speed[024] = 90;
-//    pos_x[025] = 037059; pos_y[025] = 001704; pos_z[025] = 00000; speed[025] = 90;
-//    pos_x[026] = 050000; pos_y[026] = 000000; pos_z[026] = 00000; speed[026] = 90;
-//    pos_x[027] = 050000; pos_y[027] = 000000; pos_z[027] = 05000; speed[027] = 40;
-//    pos_x[028] = 000000; pos_y[028] = 000000; pos_z[028] = 05000; speed[028] = 40;
-//    pos_x[029] = 000000; pos_y[029] = 000000; pos_z[029] = 00000; speed[029] = 40;
-    
     end
 endmodule
--- Zedboard-CNC/Zedboard-CNC.srcs/sources_1/new/XYZ_Axis_Coordinator.v
+++ Zedboard-CNC/Zedboard-CNC.srcs/sources_1/new/XYZ_Axis_Coordinator.v
@@ -1,23 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: The Technion
-// Engineer: Amir Zuabi & Alex Vassiliev
-// 
-// Create Date: 06/20/2025 12:17:40 PM
-// Design Name: 
-// Module Name: XYZ_Axis_Coordinator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module XYZ_Axis_Coordinator(
     //Inputs
     input clk,
@@ -31,8 +11,8 @@ module XYZ_Axis_Coordinator(
     input step_feedback_y,
     input step_feedback_z,
     input [4:0] manual_move,// -x, +x, -y/z, +y/z, zswitch.
-    
-       
+
+
     //Outputs
     output reg [63:0] cycles_per_step_x,
     output reg [63:0] cycles_per_step_y,
@@ -45,7 +25,7 @@ module XYZ_Axis_Coordinator(
     output reg [7:0] target_speed,
     output reg signed [31:0] direction_change_buffer
 );
-    
+
     //constants
     localparam MAX_SPEED = 100;
     localparam CYCLES_PER_SECOND = 100000000;
@@ -57,22 +37,22 @@ module XYZ_Axis_Coordinator(
     localparam REVERSE_DIR_Z = 0;
     localparam HOMING_SPEED_XY = 10;
     localparam HOMING_SPEED_Z = 5;
-    
-    
+
+
     //1 mm = 1000 microns = [1000/MICRONS_PER_STEP_#] steps
     //'D' mm/second --> 1/D second/mm = 1 second / [D*1000/MICRONS_PER_STEP_#] steps
     // --> 100M clk cycles / [D*1000/MICRONS_PER_STEP_#] steps
     // --> 1 step every [100000/D]*MICRONS_PER_STEP_#
-    
+
     //calculated X/Y separately as MICRONS_PER_STEP can differ
     localparam HOMING_CYCLES_PER_STEP_X = (100000/HOMING_SPEED_XY)*MICRONS_PER_STEP_X;
     localparam HOMING_CYCLES_PER_STEP_Y = (100000/HOMING_SPEED_XY)*MICRONS_PER_STEP_Y;
-    localparam HOMING_CYCLES_PER_STEP_Z = (100000/HOMING_SPEED_Z )*MICRONS_PER_STEP_Z;    
-    
+    localparam HOMING_CYCLES_PER_STEP_Z = (100000/HOMING_SPEED_Z )*MICRONS_PER_STEP_Z;
+
     localparam ACCEL_CLK_TIMER = 50000;
     localparam MAX_INSTANT_SPEED_CHANGE = 10;
     localparam DELTA_SPEED = 1;
-    
+
     //statemachine values
     localparam INIT     = 0;
     localparam HOMING   = 1;
@@ -81,11 +61,11 @@ module XYZ_Axis_Coordinator(
     localparam CALC_1   = 4;
     localparam CALC_2   = 5;
     localparam MOVE     = 6;
-    
+
     reg [2:0] state;
     reg [31:0] state_timer;
     reg [31:0] clk_counter;
-    
+
     wire [31:0] distance_x;
     wire [31:0] distance_y;
     wire [31:0] distance_z;
@@ -101,7 +81,7 @@ module XYZ_Axis_Coordinator(
     reg signed [31:0] target_pos_x;
     reg signed [31:0] target_pos_y;
     reg signed [31:0] target_pos_z;
-    
+
     //reg [7:0] target_speed;
     //reg [7:0] current_speed;
     wire x_reached, y_reached, z_reached;
@@ -110,42 +90,42 @@ module XYZ_Axis_Coordinator(
     wire [63:0] cycles_per_step_z_result;
     reg [63:0] move_clk_cycles_counter;
     //reg signed [31:0] direction_change_buffer;
-    
-    
+
+
     assign distance_x = (target_pos_x >= start_pos_x) ? (target_pos_x - start_pos_x) : (start_pos_x - target_pos_x);
     assign distance_y = (target_pos_y >= start_pos_y) ? (target_pos_y - start_pos_y) : (start_pos_y - target_pos_y);
     assign distance_z = (target_pos_z >= start_pos_z) ? (target_pos_z - start_pos_z) : (start_pos_z - target_pos_z);
     assign squared_distance = (distance_x * distance_x) + (distance_y * distance_y) + (distance_z * distance_z);
-    
-    
+
+
     //calculations happen in the relevant states, otherwise holds 0 to not pointlessly do 64bit mul/div operations
-    
+
     assign cycles_per_step_x_result = ((distance_x > 0 && current_speed > 0) && (state == CALC_2 || state == MOVE)) ? ((MICRONS_PER_STEP_X * 100000 * distance) / (current_speed * distance_x)) : 0;
     assign cycles_per_step_y_result = ((distance_y > 0 && current_speed > 0) && (state == CALC_2 || state == MOVE)) ? ((MICRONS_PER_STEP_Y * 100000 * distance) / (current_speed * distance_y)) : 0;
     assign cycles_per_step_z_result = ((distance_z > 0 && current_speed > 0) && (state == CALC_2 || state == MOVE)) ? ((MICRONS_PER_STEP_Z * 100000 * distance) / (current_speed * distance_z)) : 0;
-    
-    
+
+
     assign x_reached = (curr_pos_x > target_pos_x - MICRONS_PER_STEP_X && curr_pos_x < target_pos_x + MICRONS_PER_STEP_X);
     assign y_reached = (curr_pos_y > target_pos_y - MICRONS_PER_STEP_Y && curr_pos_y < target_pos_y + MICRONS_PER_STEP_Y);
     assign z_reached = (curr_pos_z > target_pos_z - MICRONS_PER_STEP_Z && curr_pos_z < target_pos_z + MICRONS_PER_STEP_Z);
-    
-    
+
+
     wire signed [31:0] midpoint_x;
     wire signed [31:0] midpoint_y;
     wire signed [31:0] midpoint_z;
     wire passed_midpoint_x, passed_midpoint_y, passed_midpoint_z;
     wire passed_midpoint;
-    
+
     assign midpoint_x = (target_pos_x + start_pos_x) >> 1;
     assign midpoint_y = (target_pos_y + start_pos_y) >> 1;
     assign midpoint_z = (target_pos_z + start_pos_z) >> 1;
-    
+
     assign passed_midpoint_x = (target_pos_x >= start_pos_x) ? (curr_pos_x >= midpoint_x - MICRONS_PER_STEP_X) : (curr_pos_x <= midpoint_x + MICRONS_PER_STEP_X);
     assign passed_midpoint_y = (target_pos_y >= start_pos_y) ? (curr_pos_y >= midpoint_y - MICRONS_PER_STEP_Y) : (curr_pos_y <= midpoint_y + MICRONS_PER_STEP_Y);
     assign passed_midpoint_z = (target_pos_z >= start_pos_z) ? (curr_pos_z >= midpoint_z - MICRONS_PER_STEP_Z) : (curr_pos_z <= midpoint_z + MICRONS_PER_STEP_Z);
-    
+
     assign passed_midpoint = passed_midpoint_x && passed_midpoint_y && passed_midpoint_z;
-    
+
     always @(posedge clk) begin
         if(rst || state == INIT) begin
             target_pos_x <= 0;
@@ -153,26 +133,26 @@ module XYZ_Axis_Coordinator(
             target_pos_z <= 0;
             target_speed <= 0;
             current_speed <= 0;
-            
+
             motor_dir_x <= 0;
             motor_dir_y <= 0;
             motor_dir_z <= 0;
-            
+
             cycles_per_step_x <= 0;
             cycles_per_step_y <= 0;
             cycles_per_step_z <= 0;
-            
-            
+
+
             curr_pos_x <= 0;
             curr_pos_y <= 0;
             curr_pos_z <= 0;
             start_pos_x <= 0;
             start_pos_y <= 0;
             start_pos_z <= 0;
-            
+
             squareroot_start <= 0;
             load_next_line <= 0;
-            
+
             clk_counter <= 0;
             state_timer <= 10;
             state <= HOMING;
@@ -185,7 +165,7 @@ module XYZ_Axis_Coordinator(
                         cycles_per_step_x <=  (!manual_move[4] && (manual_move[0]^manual_move[1])) ? HOMING_CYCLES_PER_STEP_X : 0;
                         cycles_per_step_y <=  (!manual_move[4] && (manual_move[2]^manual_move[3])) ? HOMING_CYCLES_PER_STEP_Y : 0;
                         cycles_per_step_z <=   (manual_move[4] && (manual_move[2]^manual_move[3])) ? HOMING_CYCLES_PER_STEP_Z : 0;
-                        
+
                         motor_dir_x <= manual_move[1]^REVERSE_DIR_X;
                         motor_dir_y <= manual_move[3]^REVERSE_DIR_Y;
                         motor_dir_z <= manual_move[3]^REVERSE_DIR_Z;
@@ -196,7 +176,7 @@ module XYZ_Axis_Coordinator(
                         state <= LOAD;
                     end
                 end
-                
+
                 LOAD    : begin
                     if (enable) begin
                         target_pos_x <= next_pos_x;
@@ -204,23 +184,23 @@ module XYZ_Axis_Coordinator(
                         target_pos_z <= next_pos_z;
                         target_speed <= ((next_speed >= MAX_INSTANT_SPEED_CHANGE) ? ((next_speed <= MAX_SPEED) ? next_speed : MAX_SPEED) : MAX_INSTANT_SPEED_CHANGE);
                         current_speed <= MAX_INSTANT_SPEED_CHANGE;
-                        
+
                         start_pos_x <= curr_pos_x;
                         start_pos_y <= curr_pos_y;
                         start_pos_z <= curr_pos_z;
-                        
+
                         motor_dir_x <= (next_pos_x >= curr_pos_x)^REVERSE_DIR_X;
                         motor_dir_y <= (next_pos_y >= curr_pos_y)^REVERSE_DIR_Y;
                         motor_dir_z <= (next_pos_z >= curr_pos_z)^REVERSE_DIR_Z;
-                        
+
                         load_next_line <= 1; //signal to GCODE Parser that it can load the next line
-                        
+
                         clk_counter <= 0;
                         state_timer <= 256;
                         state <= CALC_0;
                     end else state <= INIT; //previous GCODE was the last, resetting and returning to HOMING mode
                 end
-                
+
                 CALC_0  : begin //delay to ensure squared_distance comb is stable
                     if (clk_counter < state_timer) begin
                         clk_counter <= clk_counter + 1;
@@ -230,7 +210,7 @@ module XYZ_Axis_Coordinator(
                         state <= CALC_1;
                     end
                 end
-                
+
                 CALC_1  : begin
                     if (result_ready) begin
                         clk_counter <= 0;
@@ -238,7 +218,7 @@ module XYZ_Axis_Coordinator(
                         state <= CALC_2;
                     end
                 end
-                
+
                 CALC_2  : begin
                     if (clk_counter < state_timer) begin
                         clk_counter <= clk_counter + 1;
@@ -246,7 +226,7 @@ module XYZ_Axis_Coordinator(
                         cycles_per_step_x <= cycles_per_step_x_result;
                         cycles_per_step_y <= cycles_per_step_y_result;
                         cycles_per_step_z <= cycles_per_step_z_result;
-                        
+
                         clk_counter <= 0;
                         move_clk_cycles_counter <= 0;
                         direction_change_buffer <= 0;
@@ -254,19 +234,19 @@ module XYZ_Axis_Coordinator(
                         state <= MOVE;
                     end
                 end
-                
+
                 MOVE    : begin
                     move_clk_cycles_counter <= move_clk_cycles_counter + 1;
                     if (current_speed > target_speed) current_speed <= target_speed;
                     else if (current_speed < MAX_INSTANT_SPEED_CHANGE) current_speed <= MAX_INSTANT_SPEED_CHANGE;
-                    
+
                     if (move_clk_cycles_counter > ACCEL_CLK_TIMER) begin
                         move_clk_cycles_counter <= 0;
-                        
+
                         if (!x_reached) cycles_per_step_x <= cycles_per_step_x_result;
                         if (!y_reached) cycles_per_step_y <= cycles_per_step_y_result;
                         if (!z_reached) cycles_per_step_z <= cycles_per_step_z_result;
-                        
+
                         if (!passed_midpoint) begin
                             if (current_speed < target_speed) begin
                                 current_speed <= current_speed + DELTA_SPEED;
@@ -279,16 +259,16 @@ module XYZ_Axis_Coordinator(
                             end
                         end
                     end
-                    
-                    
+
+
                     if (step_feedback_x) curr_pos_x <= curr_pos_x + ((target_pos_x >= start_pos_x) ? MICRONS_PER_STEP_X : -MICRONS_PER_STEP_X);
                     if (step_feedback_y) curr_pos_y <= curr_pos_y + ((target_pos_y >= start_pos_y) ? MICRONS_PER_STEP_Y : -MICRONS_PER_STEP_Y);
                     if (step_feedback_z) curr_pos_z <= curr_pos_z + ((target_pos_z >= start_pos_z) ? MICRONS_PER_STEP_Z : -MICRONS_PER_STEP_Z);
-                    
+
                     if (x_reached) cycles_per_step_x <= 0;
                     if (y_reached) cycles_per_step_y <= 0;
                     if (z_reached) cycles_per_step_z <= 0;
-                    
+
                     if (clk_counter >= state_timer || (x_reached && y_reached && z_reached)) begin
                         clk_counter <= 0;
                         state_timer <= 10;
@@ -300,11 +280,11 @@ module XYZ_Axis_Coordinator(
             endcase
         end
     end
-    
+
     //iterative function for square root
     reg [63:0] val, res, bit;
     reg [6:0] i;
-    
+
     always @(posedge clk) begin
         if (rst) begin
             val <= 0;
@@ -344,7 +324,7 @@ module XYZ_Axis_Coordinator(
             end
         end
     end
-    
+
 
 
 
