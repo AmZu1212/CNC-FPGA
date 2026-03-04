95db7ef4da0cbd5a81204106a5bcae7b113cde92
Amir Zuabi <zuabiamir123@gmail.com>
2026-03-04 15:42:08 +0200

    hex file re-order

---
+++ Hexfiles/set file type.tcl
@@ -0,0 +1,4 @@
+set_property FILE_TYPE {Memory File} [get_files pos_x.hex]
+set_property FILE_TYPE {Memory File} [get_files pos_y.hex]
+set_property FILE_TYPE {Memory File} [get_files pos_z.hex]
+set_property FILE_TYPE {Memory File} [get_files speed.hex]
