ccba50e33e8b2961cab323f473a5b195719c9fac
Amir Zuabi <zuabiamir123@gmail.com>
2026-03-11 22:40:15 +0200

    removed old example folder

--- Vitis-SD-Example/Vitis-Platforms/_ide/workspace_journal_202572.py
+++
@@ -1,8 +0,0 @@
-# 2025-12-25T22:59:40.744411300
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="Vitis-Platforms")
-
-vitis.dispose()
-
--- Vitis-SD-Example/Vitis-Platforms/_ide/workspace_journal_58708.py
+++
@@ -1,8 +0,0 @@
-# 2025-12-24T13:40:13.743337700
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="Vitis-Platforms")
-
-vitis.dispose()
-
